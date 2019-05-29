c0447825c7cb2047d599713f1b2beba9f1010d6e
ryochin0114 <ryochin@kwansei.ac.jp>
2019-05-29 20:43:48 +0900

    試作品Version1.2

--- app/src/main/java/com/example/eigo/MainActivity.java
+++ app/src/main/java/com/example/eigo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eigo;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.ActivityNotFoundException;
@@ -10,24 +11,30 @@ import android.widget.Button;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Locale;
+import android.app.Activity;
+import java.util.Queue;
+import android.text.SpannableStringBuilder;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
+import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener {
     // リクエストを認識するための変数宣言。適当な数字でいい
     private static final int REQUEST_CODE = 1000;
     private TextView textView;
-
-
+    private TextToSpeech tts;
+    private static final String TAG = "TestTTS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        tts = new TextToSpeech(this,this);
 
         textView = (TextView)findViewById(R.id.text_view);
-
         Button buttonStart = (Button)findViewById(R.id.button_start);
         buttonStart.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -35,11 +42,109 @@ public class MainActivity extends AppCompatActivity {
                 speech();
             }
         });
+        Button ttsButton = findViewById(R.id.button_start2);
+        ttsButton.setOnClickListener(this);
+
     }
+    public void onInit(int status) {
+        // TTS初期化
+        if (TextToSpeech.SUCCESS == status) {
+            Log.d(TAG, "initialized");
+        } else {
+            Log.e(TAG, "failed to initialize");
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        speechText();
+    }
+    private void shutDown(){
+        if (null != tts) {
+            // to release the resource of TextToSpeech
+            tts.shutdown();
+        }
+    }
+    private void speechText() {
+        TextView editor = findViewById(R.id.text_view2);
+
+        // EditTextからテキストを取得
+        String string = editor.getText().toString();
+
+        if (0 < string.length()) {
+            if (tts.isSpeaking()) {
+                tts.stop();
+                return;
+            }
+            setSpeechRate(1.0f);
+            setSpeechPitch(1.0f);
+
+            if (Build.VERSION.SDK_INT >= 21){
+                // SDK 21 以上
+                tts.speak(string, TextToSpeech.QUEUE_FLUSH, null, "messageID");
+            }
+            else{
+                // tts.speak(text, TextToSpeech.QUEUE_FLUSH, null) に
+                // KEY_PARAM_UTTERANCE_ID を HasMap で設定
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"messageID");
+                tts.speak(string, TextToSpeech.QUEUE_FLUSH, map);
+            }
+
+            setTtsListener();
+        }
+    }
+    private void setSpeechRate(float rate){
+        if (null != tts) {
+            tts.setSpeechRate(rate);
+        }
+    }
+
+    // 読み上げのピッチ
+    private void setSpeechPitch(float pitch){
+        if (null != tts) {
+            tts.setPitch(pitch);
+        }
+    }
+
+    // 読み上げの始まりと終わりを取得
+    private void setTtsListener(){
+        // android version more than 15th
+        if (Build.VERSION.SDK_INT >= 15){
+            int listenerResult =
+                    tts.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                        @Override
+                        public void onDone(String utteranceId) {
+                            Log.d(TAG,"progress on Done " + utteranceId);
+                        }
+
+                        @Override
+                        public void onError(String utteranceId) {
+                            Log.d(TAG,"progress on Error " + utteranceId);
+                        }
+
+                        @Override
+                        public void onStart(String utteranceId) {
+                            Log.d(TAG,"progress on Start " + utteranceId);
+                        }
+                    });
+
+            if (listenerResult != TextToSpeech.SUCCESS) {
+                Log.e(TAG, "failed to add utterance progress listener");
+            }
+        }
+        else {
+            Log.e(TAG, "Build VERSION is less than API 15");
+        }
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        shutDown();
+    }
+
     private void speech(){
         //音声認識プロンプトを立ち上げるインテント作成
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
 
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH.toString());
             intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,100);
@@ -75,3 +180,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
